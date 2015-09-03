7e5eedfaa2db69f2fde654724a6c198619b2b448
Nathan Forsyth <nathancraigforsyth@gmail.com>
2015-09-03 16:42:04 +1200

    Initial commit for CPP Collaboration project

---
+++ DelegatesAndEventsDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEventsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
