0cf5f35260671c2c7a355b58ed869f2da465db73
Josh Lloyd <j.nevercast@gmail.com>
2015-09-07 17:36:36 +1200

    Make Person async and use Component Model
    - Remove ValueChangeEventHandler delegate
    - Rename ValueChangedEventArgs to PropertyValueChangedEventArgs
    - PropertyValueChangedEventArgs extends PropertyChangedEventArgs from
    System.ComponentModel
    - Person implements INotifyPropertyChanged from ComponentModel and can
    now be used for Data Binding
    - Redundant property code moved to OnPropertyValueChanged method
    - OnPropertyValueChanged method invokes the PropertyChanged event from
    the TaskScheduler
    - Errors from event calls are handled softly and asyncronously
    
    Events are called asynchronously now from tasks, meaning the are out of
    order on the example program. This is the biggest change.

--- DelegatesAndEventsDemo/Program.cs
+++ DelegatesAndEventsDemo/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -14,31 +15,61 @@ namespace DelegatesAndEventsDemo
         static void Main(string[] args)
         {
             // create a person and display the parameters to the console
-            Person p = new Person() { FirstName = "Nathan", LastName = "Forsyth", Age = 100 };
-            Console.WriteLine(p.ToString() + "\r\n");
+            var p = new Person("Nathan", "Forsyth", 100);
+            LogPerson(p);
 
-            p.ValueChanged += p_ValueDelegate; // program is now subscribed to the value change event handler
+            p.PropertyChanged += p_ValueDelegate; // program is now subscribed to the value change event handler
 
             p.FirstName = "John";
             p.LastName = "Wang";
             p.Age = 99;
-
-            Console.WriteLine();
+            LogPerson(p);
 
             p.FirstName = "Ray";
             p.LastName = "Singer";
             p.Age = 98;
+            LogPerson(p);
 
             Console.Read();
         }
 
-        private static void p_ValueDelegate(object sender, ValueChangedEventArgs args)
+        /// <summary>
+        /// Logs a person object, also handles null
+        /// </summary>
+        /// <param name="p">Person to log to Console</param>
+        static void LogPerson(Person p)
         {
-#if BASIC // comment/uncomment first line to switch logic path
-            // basic event handling
-            Console.WriteLine("{0} changed to {1}", args.OldValue, args.NewValue);
-            Console.WriteLine(sender.ToString()); // call overridden ToString() on the sender object (person in this case)
-#else
+            if (p == null)
+            {
+                Console.WriteLine("LogPerson(null)\r\n");
+            }
+            else
+            {
+                Console.WriteLine("{0}\r\n", p);
+            }
+        }
+
+        // This is invoked asyncronously!!
+        private static void p_ValueDelegate(object sender, PropertyChangedEventArgs args)
+        {
+            // Create a dynamic object that references the arguments
+            // This allows us to use .OldValue and .NewValue without casting if the type matches
+            // This is probably bad practice or something. 
+            dynamic ooohDynamic = args;
+
+            // If we have the values, log them
+            if (ooohDynamic is PropertyValueChangedEventArgs)
+            {
+                Console.WriteLine("Property {0} changed from {1} to {2}", ooohDynamic.PropertyName, ooohDynamic.OldValue, ooohDynamic.NewValue);
+            }
+            else // Otherwise we only have the property name
+            {
+                Console.WriteLine("Property {0} changed.", ooohDynamic.PropertyName);
+            }
+
+            // Somewhat obsoleted by PropertyChangedEvent. We need to find a new use for Reflection! :D
+
+            /*
             // more advanced event handling; allows for reflection inside the object to get property NAME, the value of which is compared to the argument value
             PropertyInfo[] properties = sender.GetType().GetProperties();
             string changedProperty = "";
@@ -53,7 +84,7 @@ namespace DelegatesAndEventsDemo
 
             Console.WriteLine("{0} changed from {1} to {2}", changedProperty, args.OldValue, args.NewValue);
             Console.WriteLine(sender.ToString());
-#endif
+            */
         }
     }
 }
